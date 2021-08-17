c8ad8be55b48da56665ffbdd925d9242392c38e0
Todd Albert <todd@bocacode.com>
2021-08-17 10:56:16 -0400

    added protected route

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import { Form, Input, Button } from 'antd'
+import { useState } from 'react'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import ProtectedForm from './components/ProtectedForm'
 import 'antd/dist/antd.css'
 
 export default function App() {
-  function handleSignup({ email, password }) {
-    fetch('https://auth-hash-api-bc.web.app/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    })
-      .then(response => response.json())
-      .then(data => alert(data.message))
-      .catch(err => alert(err))
+  const [user, setUser] = useState()
+  const [returningUser, setReturningUser] = useState(false)
+  if(!user) {
+    return (
+      <>
+        {!returningUser
+          ? <Signup setUser={setUser} setReturningUser={setReturningUser} />
+          : <Login setUser={setUser} setReturningUser={setReturningUser} />
+        }
+      </>
+    )
   }
-  return (
-    <Form
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      onFinish={handleSignup}
-    >
-      <h1>Sign up</h1>
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your email!' }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">Sign up</Button>
-      </Form.Item>
-    </Form>
-  )
+  return <ProtectedForm />
 }
